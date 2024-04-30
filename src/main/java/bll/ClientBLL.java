536864a0306ce551bd111bd90c28aa25b52dff8d
Daniel <danielrad111@gmail.com>
2024-04-30 19:56:05 +0300

    First commit

---
+++ src/main/java/bll/ClientBLL.java
@@ -0,0 +1,33 @@
+package bll;
+
+import bll.validators.ClientAgeValidator;
+import bll.validators.EmailValidator;
+import bll.validators.Validator;
+import dao.ClientDAO;
+import model.Client;
+
+import java.util.*;
+
+public class ClientBLL {
+    private List<Validator<Client>> validators;
+    public ClientBLL(){
+        validators = new ArrayList<Validator<Client>>();
+        validators.add(new EmailValidator());
+        validators.add(new ClientAgeValidator());
+    }
+
+    public Client findClientById(int id){
+        Client client = ClientDAO.findById(id);
+        if(client == null){
+            throw new NoSuchElementException("The client with id " + id + " does not exist");
+        }
+        return client;
+    }
+
+    public int insertClient(Client client){
+        for(Validator<Client> validator : validators){
+            validator.validate(client);
+        }
+        return ClientDAO.insert(client);
+    }
+}
